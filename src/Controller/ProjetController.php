0ee7eede341d4acb3b868ab750dd02ba22b4cbff
Max Pochet <max.pochet@orange.fr>
2022-01-20 11:45:36 +0100

    preparation pour les projets avec bdd

---
+++ src/Controller/ProjetController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Projet;
+use App\Form\ProjetType;
+use App\Repository\ProjetRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/projet')]
+class ProjetController extends AbstractController
+{
+    #[Route('/', name: 'projet_index', methods: ['GET'])]
+    public function index(ProjetRepository $projetRepository): Response
+    {
+        return $this->render('projet/index.html.twig', [
+            'projets' => $projetRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'projet_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $projet = new Projet();
+        $form = $this->createForm(ProjetType::class, $projet);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($projet);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('projet_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('projet/new.html.twig', [
+            'projet' => $projet,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'projet_show', methods: ['GET'])]
+    public function show(Projet $projet): Response
+    {
+        return $this->render('projet/show.html.twig', [
+            'projet' => $projet,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'projet_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Projet $projet, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(ProjetType::class, $projet);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('projet_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('projet/edit.html.twig', [
+            'projet' => $projet,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'projet_delete', methods: ['POST'])]
+    public function delete(Request $request, Projet $projet, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$projet->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($projet);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('projet_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
